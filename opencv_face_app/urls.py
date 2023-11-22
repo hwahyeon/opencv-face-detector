0335745c0a84ed122f9705680be3d0ef9548f535
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-23 02:18:28 +0900

    Added the page for image upload

--- opencv_face_app/urls.py
+++ opencv_face_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
-from . import views
+from . import views  # add
+from django.conf import settings # add
+from django.conf.urls.static import static # add
 
 urlpatterns = [
     re_path(r'^$', views.first_view, name='first_view'),
+    re_path(r'^uimage/$', views.uimage, name='uimage'), # add
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
